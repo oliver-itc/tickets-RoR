4fd0d2958968f7721786e43d6d9e88bd0aff8483
oliver-itc <oliver-itc@hushmail.com>
2013-08-29 22:50:53 -0400

    add phone label and string to htmls

--- db/migrate/20130830023236_add_phone_to_tickets.rb
+++ db/migrate/20130830023236_add_phone_to_tickets.rb
@@ -1,5 +1,5 @@
 class AddPhoneToTickets < ActiveRecord::Migration
   def change
-    add_column :tickets, :pone, :string
+    add_column :tickets, :phone, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130829073409) do
+ActiveRecord::Schema.define(version: 20130830023236) do
 
   create_table "tickets", force: true do |t|
     t.string   "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20130829073409) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "pone"
+    t.string   "phone"
   end
 
 end
